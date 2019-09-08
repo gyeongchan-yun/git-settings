66bfc1bc7aa675b7103ba5184fd1066ecbd835dd
Gyeongchan Yun <rugyoon@gmail.com>
2019-09-08 22:31:06 +0900

    MODIFY setup.sh to change config with mode (global or local)

--- setup.sh
+++ setup.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
+function check_and_rename_file() {
+  if [ -f $1 ]; then
+    echo "[WARNING] $1 already exists."
+    mv $1 $1_temp
+    echo "[INFO] Rename $1 to ${1}_temp"
+  fi
+}
+
 dst=$1
 
 if [ -z $dst ]; then
-    echo "USAGE: [destination path]"
-    exit 1
+  echo "USAGE: [destination path]"
+  exit 1
 fi
 
 if [ ! -d $dst ]; then
-    echo "[ERROR] Destination directory doesn't exists."
-    exit 1
+  echo "[ERROR] Destination directory doesn't exists."
+  exit 1
 fi
 
-cp .gitconfig $dst
-echo "successfully copy .gitconfig to $dst"
-cp .gitignore $dst
-echo "successfully copy .gitignore to $dst"
+if [ $dst == $HOME ]; then
+  echo "[INFO] git config mode is GLOBAL."
+  check_and_rename_file $dst/.gitconfig
+  check_and_rename_file $dst/.gitignore
+  cp .gitconfig $dst
+  echo "successfully copy .gitconfig to $dst"
+  cp .gitignore $dst
+  echo "successfully copy .gitignore to $dst"
+else
+  echo "[INFO] git config mode is LOCAL."
+  if [ ! -d $dst/.git ]; then
+    echo "[ERROR] Destination directory is not working git directory."
+    exit 1
+  fi
+  check_and_rename_file $dst/.git/config
+  check_and_rename_file $dst/.gitignore
+  cp .gitconfig $dst/.git/config
+  echo "successfully copy .gitconfig to $dst"
+  cp .gitignore $dst
+  echo "successfully copy .gitignore to $dst"
+fi
